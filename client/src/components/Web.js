67a8ec777ebb1b900b33e4036306e56b15ba8a60
sean stephen brian <108032222+seanstephenbrian@users.noreply.github.com>
2023-06-23 17:34:02 -0400

    retrieve web project info from local json file

--- client/src/components/Web.js
+++ client/src/components/Web.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import uniqid from 'uniqid';
+
+import '../styles/web.scss';
 
 export default function Web() {
+    
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        fetch('./projects.json')
+            .then(res => res.json())
+            .then((data) => {
+                setProjects(data.projects);
+            });
+    }, []);
+
     return (
-        'web'
+        <section className='web'>
+            {projects.map((project) => {
+                return (
+                    <div className='web-project' key={uniqid()}>
+                        {project.title}
+                    </div>
+                )
+            })}
+        </section>
     )
 }
