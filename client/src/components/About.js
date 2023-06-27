3e5f97a70356d347e29e0ae09490b66c3ac3b3b2
sean stephen brian <108032222+seanstephenbrian@users.noreply.github.com>
2023-06-27 15:59:32 -0400

    style tweaks for better mobile performance

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,7 +5,7 @@ import '../styles/about.scss';
 export default function About() {
 
     return (
-        <section className='about'>
+        <section className='about-section'>
             <p>thanks for stopping by!</p>
             <p>
                 if you like what you see, need help on a project, or just want to connect,
