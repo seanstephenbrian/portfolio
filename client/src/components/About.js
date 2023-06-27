52d4c10bd4daf9270e8203a551b29843b64fe5cc
sean stephen brian <108032222+seanstephenbrian@users.noreply.github.com>
2023-06-27 19:57:32 -0400

    .

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -9,7 +9,7 @@ export default function About() {
             <p className='thanks'>thanks for stopping by!</p>
             <p>
                 if you like what you see, need help on a project, or just want to connect,
-                you can reach me by email at <span style={{ fontWeight: '375' }}>seanstephenbrian@gmail.com</span>
+                you can reach me by email at <span style={{ fontWeight: '375' }}>seanstephenbrian@gmail.com</span>.
             </p>
         </section>
     )
