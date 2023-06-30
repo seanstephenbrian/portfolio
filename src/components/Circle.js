4c9c0d01174288e4ff29f88914b89592e45c1464
sean stephen brian <seanstephenbrian@gmail.com>
2023-06-30 13:45:39 -0400

    modify spinning contact wheel

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -10,21 +10,18 @@ import GitHubIcon from '../assets/icons/github.svg';
 export default function Circle() {
     return (
         <div className='circle'>
-            { ['q1', 'q2', 'q3', 'q4'].map((quadrant) => {
-                return (
-                    <div className={quadrant} key={uniqid()}>
-                        <a href='https://www.instagram.com/seanstephenbrian/' target='_blank' rel='noopener noreferrer'>
-                            <img className='icon insta' src={InstaIcon} alt='Instagram'></img>
-                        </a>
-                        <a href='https://github.com/seanstephenbrian' target='_blank' rel='noopener noreferrer'>
-                            <img className='icon github' src={GitHubIcon} alt='GitHub'></img>
-                        </a>
-                        <a href='mailto:seanstephenbrian@gmail.com'>
-                            <img className='icon email' src={EmailIcon} alt='Email'></img>
-                        </a>
-                    </div>
-                )
-            })}
+            <a href='https://www.instagram.com/seanstephenbrian/' target='_blank' rel='noopener noreferrer'>
+                <img className='icon insta' src={InstaIcon} alt='Instagram'></img>
+            </a>
+            <a href='https://github.com/seanstephenbrian' target='_blank' rel='noopener noreferrer'>
+                <img className='icon github' src={GitHubIcon} alt='GitHub'></img>
+            </a>
+            <a href='mailto:seanstephenbrian@gmail.com'>
+                <img className='icon email email-1' src={EmailIcon} alt='Email'></img>
+            </a>
+            <a href='mailto:seanstephenbrian@gmail.com'>
+                <img className='icon email email-2' src={EmailIcon} alt='Email'></img>
+            </a>
         </div>
     )
 }
